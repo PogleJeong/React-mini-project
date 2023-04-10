f2eb60069b60dbe1129731863c81b74ad3c2e52c
jes <jes4958@yonsei.ac.kr>
2023-04-11 00:20:02 +0900

    add styled-component animations

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './Styled Component/part_2';
+import App from './Styled Component/part_4';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
